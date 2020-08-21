16bbd6637857a812858643f35ce0360591662fbc
greedazgood <sunmail93@163.com>
2020-08-21 21:34:07 +0800

    fix:模板调整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,10 +15,10 @@
         <div class="col-md-4">
             <div class="right-panel">
                 <div class="col-md-12">
-                    <input type="text" style="width: 80%" placeholder="Search for...">
-                    <button style="width: 18%">Search</button>
+                    <input class="search-input" type="text" placeholder="Search for...">
+                    <button class="search-button">Search</button>
                 </div>
-                <div class="col-md-12" style="margin-top: 20px;">
+                <div class="col-md-12 right-panel-box">
                     <div class="panel-title">分类：</div>
                     <ul class="cat_list">
                         @foreach($data['cat_list'] as $cat)
@@ -26,19 +26,18 @@
                         @endforeach
                     </ul>
                 </div>
+                <div class="col-md-12 right-panel-box">
+                    <div class="panel-title">归档：</div>
+                    <ul class="cat_list">
+                        <li><a href="#">2020-08</a></li>
+                        <li><a href="#">2020-08</a></li>
+                        <li><a href="#">2020-08</a></li>
+                        <li><a href="#">2020-08</a></li>
+                        <li><a href="#">2020-08</a></li>
+                        <li><a href="#">2020-08</a></li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-md-12" style="margin-top: 20px;">
-                <div class="panel-title">归档：</div>
-                <ul class="cat_list">
-                    <li><a href="#">2020-08</a></li>
-                    <li><a href="#">2020-08</a></li>
-                    <li><a href="#">2020-08</a></li>
-                    <li><a href="#">2020-08</a></li>
-                    <li><a href="#">2020-08</a></li>
-                    <li><a href="#">2020-08</a></li>
-                </ul>
-            </div>
-        </div>
         </div>
     </div>
 @stop
